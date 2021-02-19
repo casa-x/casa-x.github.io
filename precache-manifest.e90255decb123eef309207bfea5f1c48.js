da85aac8a5341de37ff68ebbdafd27637b798916
Marvin Guerra <me@senorllama.com>
2021-02-18 16:34:46 -0800

    Updates

---
+++ precache-manifest.e90255decb123eef309207bfea5f1c48.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e95975987ffeaf06e49f7c63097b7874",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b7fe7acceb6b7f3b4708",
+    "url": "/static/css/main.71664ee2.chunk.css"
+  },
+  {
+    "revision": "3d6abb15b653e3c04ed4",
+    "url": "/static/js/2.a553fdbf.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.a553fdbf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b7fe7acceb6b7f3b4708",
+    "url": "/static/js/main.2da6be69.chunk.js"
+  },
+  {
+    "revision": "606a755df789df113d96064098a4d937",
+    "url": "/static/js/main.2da6be69.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9084a317fdffc61a373a",
+    "url": "/static/js/runtime-main.1d650a35.js"
+  },
+  {
+    "revision": "ef3b6c9a42294b970f55a244299610e2",
+    "url": "/static/media/landingBackground.ef3b6c9a.jpg"
+  },
+  {
+    "revision": "22a0bffe789c286a9d78eb52670996a7",
+    "url": "/static/media/nucleo-outline.22a0bffe.ttf"
+  },
+  {
+    "revision": "24e2d6b43b1b0f84fdfaa06a4032f154",
+    "url": "/static/media/nucleo-outline.24e2d6b4.woff"
+  },
+  {
+    "revision": "53a1bed7a3ec86d010fe100873828a89",
+    "url": "/static/media/nucleo-outline.53a1bed7.eot"
+  },
+  {
+    "revision": "8ebec31f5ce59f908db84d86aed5947f",
+    "url": "/static/media/nucleo-outline.8ebec31f.woff2"
+  },
+  {
+    "revision": "f57b29b2e87016f3f53e23b56ca73f0e",
+    "url": "/static/media/teamworkBackground.f57b29b2.jpg"
+  }
+]);
